6436ad50aaefc8af660e221e01d0d4b76b61f4c4
Ellen Dunn <edunn227@gmail.com>
2018-06-27 16:27:23 -0500

    started handleSearchResults and added map

--- index.js
+++ index.js
@@ -4,38 +4,54 @@ const ZIPCODE_ENDPOINT = 'https://search.ams.usda.gov/farmersmarkets/v1/data.svc
 const FM_ID_ENDPOINT = 'https://search.ams.usda.gov/farmersmarkets/v1/data.svc/mktDetail?id='
 
 
-function gatherResultsFromApi(query, callback){
+function gatherResultsFromApi(query, endpoint, callback) {
 	// this function will gather the results from the API based on the input from the user and renderResults() 
 	//on the map
 	const settings = {
 		// should this url be ZIPCODE_ENDPOINT+query/zip entered?
-		url: ZIPCODE_ENDPOINT + query,
+		url: endpoint + query,
 		type:'GET',
 		contentType: "application/json; charset=utf-8",
 		data: {
-			zip: query,
+			q: query,
 			per_page: 10
 		},
-		success: callback,
+		dataType: 'jsonp',
+        jsonpCallback: callback,
 	}
 	$.ajax(settings)
 
-	console.log('results are gathered')
+	// console.log()
 
 }
 
-function getResultData(){
+function handleSearchResults(data){
+	// this will interpret results from API to then put in showResultsOnMap
+	const resultsElem = $('.results');
 
+	$('#map').prop('hidden', false);
+
+	resultsElem
+ 	.prop('hidden', false)
+ 	// .html(resultsHtml); <-- this needs to be adding it to the map?
+
+	const results = data.results.map((item, index) =>
+		showResultsOnMap(item));
+	console.log(results)
+
+}
+
+let map;
+
+function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: -34.397, lng: 150.644},
+    zoom: 8
+    });
 }
 
 function showResultsOnMap(){
 	// this will load the map of results to the page
-	const resultsElem = $('.results');
-
-	outputElem
-    .prop('hidden', false)
-    // .html(resultsHtml);
-
 	console.log('results are loaded to page')
 }
 
@@ -47,24 +63,31 @@ function handleSubmitButton(){
 		const queryTarget = $(event.currentTarget).find('#zip-search');
 		const zip = queryTarget.val();
 		queryTarget.val('');
-		gatherResultsFromApi(zip, getResultData);
+		gatherResultsFromApi(zip, ZIPCODE_ENDPOINT, handleSearchResults(zip)); //experimentally entered argument for handleSearchResults
 		}
 	)
 }
 
 
 // after map loads, this will handle how the user interacts with the map and results
-function renderMarketDetails(){
+function showMarketDetails(){
 	// this function will show the title of the market, the schedule, and the products sold in a side bar when a user
 	// selects a market from the map
 	console.log('market details loaded to page')
+}
+
+function handleIdDetails() {
 
 }
 
 function handleMarketSelect(){
 	// this function will handle the event of the user selecting a market from the map and will renderResultDetails()
-	console.log('market selected from map')
+	
+
+	gatherResultsFromApi(id, FM_ID_ENDPOINT, handleIdDetails)
+
+	// console.log('market selected from map')
 }
 
 handleSubmitButton();
-handleMarketSelect();
+// handleMarketSelect();
