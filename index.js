f0b9708f2aab920e2dfb5b7b1c6f5ad7a03463ab
Ellen Dunn <edunn227@gmail.com>
2018-07-05 13:11:37 -0500

    close markers and cleaned up code

--- index.js
+++ index.js
@@ -1,22 +1,20 @@
 'use strict'
 
-const ZIPCODE_ENDPOINT = 'http://search.ams.usda.gov/farmersmarkets/v1/data.svc/zipSearch?zip=';
-const FM_ID_ENDPOINT = 'https://search.ams.usda.gov/farmersmarkets/v1/data.svc/mktDetail?id=';
-let map;
+const BASE_ENDPOINT = 'http://search.ams.usda.gov/farmersmarkets/v1/data.svc/'
+const ZIPCODE_ENDPOINT = BASE_ENDPOINT + 'zipSearch?zip=';
+const FM_ID_ENDPOINT = BASE_ENDPOINT + 'mktDetail?id=';
+let map, infowindow;
+
 
 function getMarketData(market){
 	return $.getJSON(FM_ID_ENDPOINT + market.id)
-		.then(data => {
-			data.name = market.marketname;
-			return data
-		})
+		.then(data => Object.assign(market, data))
 }
 
 function showMarker(market, bounds){
 	// put markers on the map
 	// extend bounds based on markers	
 	// split GoogleAddress string to get coordinates for each marker
-	// console.log(market);
 	const [lat, lng] = market.marketdetails.GoogleLink
 										.split('?q=')[1].split('%20(')[0].split('%2C%20')
 										.map(Number);
@@ -26,11 +24,7 @@ function showMarker(market, bounds){
 
 	const products = market.marketdetails.Products;
 	const schedule = market.marketdetails.Schedule;
-	// console.log(marketSchedule);
-	handleMarketSelect(marketMarker, market.name, products, schedule);
-	
-	// handleMarketSelect(marketMarker, market.name, marketSchedule, marketProducts);
-	// handleSearchResults(market.name);
+	handleMarketSelect(marketMarker, market.marketname, products, schedule);
 
 }
 
@@ -43,109 +37,95 @@ function fitMap(markets) {
 	map.fitBounds(bounds);
 }
 
-function gatherResultsFromApi(query, endpoint) { //removed callback 
-	// this function will gather the results from the API based on the input from the user and renderResults() 
-	//on the map
+function gatherResultsFromApi(query, endpoint) { 
+	// this function will gather the results from the API based on the input
+	// from the user and renderResults() 
+	// on the map
 	$.getJSON(endpoint + query, data => { 
+		const firstResult = data.results[0];
+		if (firstResult.id === 'Error'){
+			return alert(firstResult.marketname)
+		}
 		const promises = data.results.map(getMarketData);
 		const numFound = data.results.length;
 		Promise.all(promises)
-			.then(markets => fitMap(markets))
-		// 		const marketSchedule = _data.marketdetails.Schedule;
-		// 		const marketProducts = _data.marketdetails.Products;
-		// 		handleMarketSelect(marketMarker, marketName, marketSchedule, marketProducts);
+			.then(markets => fitMap(markets));
 		resultsLoad(numFound);
-		loadMap()
+		$('#map').prop('hidden', false);
 	})
-}
-
-function loadMap(){
-		const mapElem = $('#map');
-		mapElem
-			.prop('hidden', false);
 }
 
 function resultsLoad(num){
 	// this will interpret results from API to then put in showResultsOnMap
 	$('.numResults').empty();
 
-	const resultsElem = $('.results');
-	resultsElem
+	$('.results')
  		.prop('hidden', false)
-		.children('h2').append(`There are ${num} Farmers' Markets in Your Neighborhood!`)}
+		.children('h2')
+		.append(`There are ${num} Farmers' Markets in Your Neighborhood!`)
+}
 
 function initMap() {
-    map = new google.maps.Map(document.getElementById('map'), {
-    // center: {lat: 41.968131, lng:-87.684336}, 
-    zoom: 15
-    });
+  map = new google.maps.Map(document.getElementById('map'), {
+	  zoom: 15
+  });
+  infowindow = new google.maps.InfoWindow();
 }
 
 function handleSubmitButton(){
 	// this function will handle the input in the form after the 
 	// submit button is pressed and will gatherResults()
-	$('.search-form').submit(event =>{
-		$('.result-details').html(`<p>Click on a market in the map to see more details!</p>`);
-
+	$('.search-form').submit(event => {
 		event.preventDefault();
+		$('.result-details')
+			.html(`<p>Click on a market in the map to see more details!</p>`);
+
 		const queryTarget = $(event.currentTarget).find('#zip-search');
 		const zip = queryTarget.val();
 		gatherResultsFromApi(zip, ZIPCODE_ENDPOINT); 
 		queryTarget.val('');
-		}
-	)
+	});
+}
+
+function editName(name){
+	return name.replace(/\d+/g,'').replace('. ', '');
 }
 
 function showMarketDetails(name, products, schedule) {
 	$('.result-details').empty();
-	  const editName = name.replace(/\d+/g,'');
-	  const editedName=editName.replace('. ', '')
+	  const editedName = editName(name);
 
     $('.result-details').append(`<h3 class='market-title'>${editedName}</h3>
     	<p class='market-schedule'>When can you go?</p>
     	<p class='market-products'>What can you purchase?</p>`)
 
    	if (schedule === " <br> <br> <br> "){
-    	$('.market-schedule').append(`<br><span class='info'> Schedule not found.</span>`)
+    	$('.market-schedule')
+    		.append(`<br><span class='info'> Schedule not found.</span>`)
     }else{
-    	const edited = schedule.replace(';<br>','');
+    	const edited = schedule.replace(/<br>/g,'');
     	$('.market-schedule').append(`<br><span class='info'>${edited}</span>`)
     }
 
-		if (products === "") {
-	    $('.market-products').append(`<br><span class='info'>Products not found.</span>`)
+		if (!products) {
+	    $('.market-products')
+	    	.append(`<br><span class='info'>Products not found.</span>`)
 	  }else{
 	    $('.market-products').append(`<br><span class='info'>${products}</span>`)
 	  }
 }
 
-
-
 function handleMarketSelect(marker, name, products, schedule){
 	// this function will handle the event of the user selecting 
 	// a market from the map and will renderResultDetails()
-	const markers = [];
-	markers.push(marker);
+	
+	const editedName = editName(name);
 
-	const editName = name.replace(/\d+/g,'');
-	const editedName=editName.replace('. ', '')
-
-	marker.infowindow = new google.maps.InfoWindow({
-    content: editedName
-  	});
-
-  google.maps.event.addListener(marker, 'click', function() {
-  	marker.infowindow.close();
-
-		this.infowindow.open(marker.get('map'), this);
-
-  	showMarketDetails(name, products, schedule);
-    
-
-})
-
-
-
+ 	google.maps.event.addListener(marker, 'click', function() {
+ 		infowindow.setContent(editedName);
+		infowindow.open(marker.get('map'), marker);
+  	showMarketDetails(name, products, schedule);   
+	})
 };
 
 handleSubmitButton();
